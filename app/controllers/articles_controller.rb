f739ddbd1949efb12aa2d5888389fd514a278863
Lyn Dee <lyn@Lyns-MacBook-Pro.local>
2016-08-09 12:32:40 -0400

    adding more routes for comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,51 @@
 class ArticlesController < ApplicationController
 
-	  def show
-      @user = current_user
-    	@article = Article.find(params[:id])
-  		end
+  def index
+    @user = current_user
+    @articles = Article.where(username: @user.username)
+  end
+
+	def show
+    @user = current_user
+    @article = Article.find(params[:id])
+  end
 
 	def new
-      @user = current_user
+    @user = current_user
+    @article = Article.new
 	end
 
-def create
-  @article = Article.new(article_params)
- 
-  @article.save
-  redirect_to @article
-end
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @user = current_user
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end 
+  end
+
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to articles_path
+  end
  
 private
   def article_params
